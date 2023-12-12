04da96d8f59a66c517351d3d7c7b6fa44def689c
BQ310 <brianquach64@gmail.com>
2023-12-11 19:03:56 -0800

    revamped objects functional clicks

--- script.js
+++ script.js
@@ -1,99 +1,100 @@
 function Gameboard() {
+    // properties
     this.board = new Array(9).fill(0);
-    
-    Gameboard.prototype.clear = function () {
+    // methods
+    Gameboard.prototype.clear = function() {
         for (let i = 0; i < 9; i++) {
             this.board[i] = 0;
         }
-    };
-
-    Gameboard.prototype.input = function(i, x) {
-        if (i > 9 || i < 0) {
+    }
+    Gameboard.prototype.input = function(idx, val) {
+        if (idx >= 0 && idx <= 8 && val < 3 && val > 0) {
+            this.board[idx] = val;
+            return true;
+        } else {
             return false;
         }
-        if (x > 2 || x <= 0) {
-            return false;
-        }
-        if (this.board[i] != 0) {
-            return false;
-        }
-        this.board[i] = x;
-        return true;
-
-    };
-
-    Gameboard.prototype.checkWinner = function() {
-        // check vertical
-        for (let i = 0; i < 3; i++) {
-            const target = this.board[i];
-            if (target != 0 && this.board[i+3] == target && this.board[i+6] == target) {
+    }
+    Gameboard.prototype.check = function() {
+        const threeSame = (id1, id2, id3) => {
+            const target = this.board[id1];
+            if (target != 0 && this.board[id2] == target && this.board[id3] == target) {
                 return target;
             }
         }
-        // check horizontal
+        let temp = 0;
+        // horizontal check
         for (let i = 0; i < 9; i += 3) {
-            const target = this.board[i];
-            if (target != 0 && this.board[i+1] == target && this.board[i+2] == target) {
-                return target;
+            temp = threeSame(i, i+1, i+2);
+            if (temp) {
+                return temp;
             }
         }
-        // check diagonal
-        let target = this.board[0];
-        if (target != 0 && this.board[4] == target && this.board[8] == target) {
-            return target;
+        // vertical check
+        for (let i = 0; i < 3; i++) {
+            temp = threeSame(i, i+3, i+6)
+            if (temp) {
+                return temp;
+            }
         }
-        target = this.board[2];
-        if (target != 0 && this.board[4] == target && this.board[6] == target) {
-            return target;
+        // diagonal check
+        temp = threeSame(2,4,6);
+        if (temp) {
+            return temp;
+        }
+        temp = threeSame(0,4,8);
+        if (temp) {
+            return temp;
         }
         return 0;
-    };
-};
-
-function Player(x) {
-    this.id = x;
-    Player.prototype.getId = function () {
-        return this.id;
-    };
-
-    Player.prototype.getMove = function () {
-        return prompt("Player " + this.getId() + " input move:");
-    };
-};
-
-function Game() {
-    this.gb = new Gameboard();
-    this.players = [new Player(1), new Player(2)];
-    this.turn_num = 0
-
-    Game.prototype.turn = function () {
-        const currPlayer = this.players[this.turn_num % 2];
-        let move, valid;
-        do {
-            move = currPlayer.getMove();
-            valid = this.gb.input(move, currPlayer.getId());
-            if (!valid) {
-                console.log("move " + move + " not valid");
-            }
-        } while (!valid);
-        console.log("Player " + currPlayer.getId() + ": " + move);
-        const res = this.gb.checkWinner();
-        this.turn_num++;
-        return res;
-    };
-
-    Game.prototype.play = function () {
-        let state = false;
-        while (!state && this.turn_num < 9) {
-            state = this.turn();
-        }
-        if (this.turn_num >= 9) {
-            console.log("Draw");
-        } else {
-            console.log("Winner is " + state);
-        }
     }
 }
 
-const gm = new Game();
-gm.play();
+function Player(n) {
+    // properties
+    this.id = n;
+}
+
+function Game() {
+    // properties
+    this.gb = new Gameboard();
+    this.players = [new Player(1), new Player(2)];
+    this.turn_num = 0;
+
+    // methods
+    Game.prototype.start = function() {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.addEventListener('click', this.getMove);
+        });
+    }
+
+    Game.prototype.getMove = (event) => {
+        const move = event.target.getAttribute("data-idx");
+        this.turn(move);
+    }
+
+    Game.prototype.turn = function(move) {
+        const currPlayer = this.players[this.turn_num % 2];
+        this.gb.input(move, currPlayer.id);
+        const res = this.gb.check();
+        this.turn_num++;
+        console.log("turn " + this.turn_num + " Player " + currPlayer.id + ": move on " + move);
+        if (res) {
+            this.end(res);
+        }
+    }
+
+    Game.prototype.end = function(winner) {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.removeEventListener('click', this.getMove);
+        });
+        this.gb.clear();
+        console.log("The winner is Player " + winner);
+    }
+
+}
+
+const game = new Game();
+game.start();
